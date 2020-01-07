47af68a15045497f7bd71fc9cd6e740d9829c954
Krishanth Rajkumar <rajkumarkl@outlook.com>
2020-01-07 15:32:54 +0100

    Projektdateien hinzufügen.

---
+++ SnowFlakesScreenSaver/frmSnowFlakesScreenSaver.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace SnowFlakesScreenSaver
+{
+    partial class SnowFlakesScreenSaver
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SnowFlakesScreenSaver));
+            this.btnLetItSnow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLetItSnow
+            // 
+            this.btnLetItSnow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLetItSnow.FlatAppearance.BorderSize = 0;
+            this.btnLetItSnow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLetItSnow.Font = new System.Drawing.Font("Courier New", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+            this.btnLetItSnow.Location = new System.Drawing.Point(360, 238);
+            this.btnLetItSnow.Name = "btnLetItSnow";
+            this.btnLetItSnow.Size = new System.Drawing.Size(75, 23);
+            this.btnLetItSnow.TabIndex = 1;
+            this.btnLetItSnow.Text = "Let it snow!";
+            this.btnLetItSnow.UseVisualStyleBackColor = true;
+            this.btnLetItSnow.Click += new System.EventHandler(this.btnLetItSnow_Click);
+            // 
+            // SnowFlakesScreenSaver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLetItSnow);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SnowFlakesScreenSaver";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Let it snow!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnLetItSnow;
+    }
+}
+
---
+++ SnowFlakesScreenSaver/frmSnowFlakesScreenSaver.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SnowFlakesScreenSaver
+{
+    public partial class SnowFlakesScreenSaver : Form
+    {
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+        Random rnd = new Random();
+        const int W = 1920;
+        const int H = 1080;
+        const int snowFlakeSize = 5;
+
+        public SnowFlakesScreenSaver()
+        {
+            InitializeComponent();
+            BackColor = Color.Blue;
+            TransparencyKey = Color.Blue;
+            TopMost = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Size = new Size(300, 300);
+            Location = new Point(810, 390);
+
+            btnLetItSnow.Visible = false;
+            btnLetItSnow.Enabled = false;
+            btnLetItSnow.BackColor = Color.DarkGray;
+            btnLetItSnow.ForeColor = Color.Black;
+            btnLetItSnow.Size = new Size(300, 300);
+            btnLetItSnow.Location = new Point(0, 0);
+            btnLetItSnow.Cursor = Cursors.Hand;
+            btnLetItSnow.Enabled = true;
+            btnLetItSnow.Visible = true;
+        }
+
+        private void btnLetItSnow_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(btnLetItSnow);
+
+            Size = new Size(W, H);
+            Location = new Point((1920 - W) / 2, (1080 - H) / 2);
+
+            int initialStyle = (int)GetWindowLongPtr(this.Handle, -20);
+            SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+
+            Timer tmr = new Timer();
+            tmr.Interval = 50;
+
+            tmr.Tick += (s, evArgs) =>
+            {
+                bool snowFlakeToRemove = false;
+                int snowFlakeIndex=0;
+                foreach (Panel snowFlake in Controls)
+                {
+                    snowFlake.Location = new Point(snowFlake.Location.X, snowFlake.Location.Y + snowFlakeSize);
+                    if (snowFlake.Location.Y > H)
+                    {
+                        snowFlakeIndex = Controls.IndexOf(snowFlake);
+                        snowFlakeToRemove = true;
+                    }
+                }
+                if (snowFlakeToRemove)
+                {
+                    Controls.RemoveAt(snowFlakeIndex);
+                }
+
+                int x = rnd.Next(W - 1);
+                Panel newSnowFlake = new Panel()
+                {
+                    BackColor = Color.White,
+                    Size = new Size(snowFlakeSize, snowFlakeSize),
+                    BorderStyle = BorderStyle.None,
+                    Location = new Point(x, 0)
+                };
+                Controls.Add(newSnowFlake);
+                newSnowFlake.BringToFront();
+            };
+
+            tmr.Start();
+        }
+    }
+}
